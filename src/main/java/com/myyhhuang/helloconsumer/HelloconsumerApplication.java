937cc90fb103f045d8833870f6b2a7d4e0107b7b
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-02-29 23:43:10 +0800

    增加Hystrix服務容錯儀表板

--- src/main/java/com/myyhhuang/helloconsumer/HelloconsumerApplication.java
+++ src/main/java/com/myyhhuang/helloconsumer/HelloconsumerApplication.java
@@ -1,16 +1,26 @@
 package com.myyhhuang.helloconsumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableCircuitBreaker
-@EnableDiscoveryClient
-@SpringBootApplication
+//@EnableHystrix
+//@SpringBootApplication
+//@EnableEurekaClient
+//@EnableHystrixDashboard
+//@EnableCircuitBreaker
+//@EnableFeignClients
+@SpringCloudApplication
 public class HelloconsumerApplication {
 
 	@Bean
@@ -23,4 +33,14 @@ public class HelloconsumerApplication {
 		SpringApplication.run(HelloconsumerApplication.class, args);
 	}
 
+	/*
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}*/
 }
--- src/main/java/com/myyhhuang/helloconsumer/controller/HelloService.java
+++ src/main/java/com/myyhhuang/helloconsumer/controller/HelloService.java
@@ -2,11 +2,8 @@ package com.myyhhuang.helloconsumer.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.LinkedList;
 
 @Service
 public class HelloService {
